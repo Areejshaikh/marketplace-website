9f61c71c15fe6033f52e6c27a186c129a348b671
Areej <areejzaheer96@gmail.com>
2025-02-02 03:39:23 +0500

    change-payment-success

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -1,34 +1,30 @@
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-interface IParams {
-  searchParams: {
-    amount: number
-  }
-}
-
-const PaymentSuccess = ({ searchParams }: IParams) => {
+const PaymentSuccess = ({ searchParams }: { searchParams: { amount?: number } }) => {
   return (
     <div className="text-center max-w-screen-xl mx-auto my-24 bg-gray-300 p-12">
       <div className="breadcrumbs text-sm">
         <ul>
           <li>
-          <Link href={'/'}>Home</Link>
+            <Link href="/">Home</Link>
           </li>
-          <li><Link href={'/categories'}>Categories</Link></li>
+          <li>
+            <Link href="/categories">Categories</Link>
+          </li>
           <li>Payment Success</li>
         </ul>
       </div>
       <div className="text-center w-full my-24 bg-gray-300 p-12">
-        <h1 className=" text-2xl font-semibold md:text-6xl pb-12">Thank you for purchasing $ {searchParams.amount}</h1>
-        <Link href='/shippingRates' >
-          <Button>GO TO Shipment</Button>
+        <h1 className="text-2xl font-semibold md:text-6xl pb-12">
+          Thank you for purchasing ${searchParams?.amount || 0}
+        </h1>
+        <Link href="/shippingRates">
+          <Button>Go To Shipment</Button>
         </Link>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default PaymentSuccess
-
+export default PaymentSuccess;
