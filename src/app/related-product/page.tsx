5277adfb7b9ad9623f271f1eb87a432f57e04f02
Areej <areejzaheer96@gmail.com>
2025-02-02 17:43:11 +0500

    update

--- src/app/related-product/page.tsx
+++ src/app/related-product/page.tsx
@@ -1,14 +1,14 @@
-import RelatedProduct from "@/components/related";
+// import RelatedProduct from "@/components/related";
 
-interface Props {
-  searchParams: {
-    category?: string;
-    currentProductId?: string;
-  };
-}
+// interface Props {
+//   searchParams: {
+//     category?: string;
+//     currentProductId?: string;
+//   };
+// }
 
-const RelatedProductPage = ({ searchParams }: Props) => {
-  return <RelatedProduct category={searchParams.category || ""} currentProductId={searchParams.currentProductId || ""} />;
-};
+// const RelatedProductPage = ({ searchParams }: Props) => {
+//   return <RelatedProduct category={searchParams.category || ""} currentProductId={searchParams.currentProductId || ""} />;
+// };
 
-export default RelatedProductPage;
+// export default RelatedProductPage;
